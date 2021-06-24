63b9ac0821bdfef231bc904d28d3376ebd22c2e1
Sergey <cabyrkih@mail.ru>
2021-06-24 21:16:56 +0300

    fix js body lock burger

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -4,6 +4,7 @@ const burgerMenu = () => {
    const headerNav = document.querySelector('.header__menu')
    const headerUl = document.querySelector('.header__list')
    const close = document.querySelector('.menu__close')
+   const body = document.querySelector('.body')
 
    function adaptivMenu(windowWidth, breakpoint) {
       if (windowWidth < breakpoint) {
@@ -29,12 +30,14 @@ const burgerMenu = () => {
       close.classList.add('active')
       burger.classList.add('active')
       menu.classList.add('active')
+      body.classList.add('lock')
    })
 
    close.addEventListener('click', () => {
       close.classList.remove('active')
       menu.classList.remove('active')
       burger.classList.remove('active')
+      body.classList.remove('lock')
    })
 
 }
